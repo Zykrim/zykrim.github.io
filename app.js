1e18e0d09511c82c761bc7753731dfcecd87e20a
Zykrim <Zykrim@users.noreply.github.com>
2018-12-03 17:17:17 -0500

    Winter 2018

--- app.js
+++ app.js
@@ -1,53 +1,25 @@
-$(document).ready(function () {
-    "use strict";
-    $('.navbtn-1').click(function () {
-        var currentSlide = $('.slide-active'), thisSlide = $('.slide-1'),
-            currentBtn = $('.navbtn-active'), thisBtn = $('.navbtn-1'),
-            otherBtn = $('.navbtn-2, .navbtn-3, .btn-4');
-        
-        if (!thisSlide.hasClass('slide-active')) {
-            currentSlide.hide(500).removeClass('slide-active');
-            thisSlide.show(500).addClass('slide-active');
-        }
-        
-        if (!thisBtn.hasClass('navbtn-active')) {
-            currentBtn.removeClass('navbtn-active');
-            thisBtn.removeClass('navbtn');
-            thisBtn.addClass('navbtn-active');
-            otherBtn.addClass('navbtn');
-        }
-    });
-    $('.navbtn-2').click(function () {
-        var currentSlide = $('.slide-active'), thisSlide = $('.slide-2'),
-            currentBtn = $('.navbtn-active'), thisBtn = $('.navbtn-2'),
-            otherBtn = $('.navbtn-1, .navbtn-3, .btn-4');
-    
-        if (!thisSlide.hasClass('slide-active')) {
-            currentSlide.hide(500).removeClass('slide-active');
-            thisSlide.show(500).addClass('slide-active');
-        }
-        
-        if (!thisBtn.hasClass('navbtn-active')) {
-            currentBtn.removeClass('navbtn-active');
-            thisBtn.removeClass('navbtn');
-            thisBtn.addClass('navbtn-active');
-            otherBtn.addClass('navbtn');
-        }
-    });
-    $('.navbtn-3').click(function () {
-        var currentSlide = $('.slide-active'), thisSlide = $('.slide-3'),
-            currentBtn = $('.navbtn-active'), thisBtn = $('.navbtn-3'),
-            otherBtn = $('.navbtn-1, .navbtn-2, .btn-4');
-    
-        if (!thisSlide.hasClass('slide-active')) {
-            currentSlide.hide(500).removeClass('slide-active');
-        }
-        
-        if (!thisBtn.hasClass('navbtn-active')) {
-            currentBtn.removeClass('navbtn-active');
-            thisBtn.removeClass('navbtn');
-            thisBtn.addClass('navbtn-active');
-            otherBtn.addClass('navbtn');
-        }
-    });
+
+$('.nav-1').click(function () {
+  $(".content").show();
+  $(".rules").hide();
+  $(".doverkids").hide()
+  $(".nav-1").addClass('nav--active')
+  $(".nav-2").removeClass('nav--active')
+  $(".nav-3").removeClass('nav--active')
+});
+$('.nav-2').click(function () {
+  $(".rules").show();
+  $(".content").hide();
+  $(".doverkids").hide();
+  $(".nav-2").addClass('nav--active')
+  $(".nav-1").removeClass('nav--active')
+  $(".nav-3").removeClass('nav--active')
+});
+$('.nav-3').click(function () {
+  $(".doverkids").show();
+  $(".content").hide();
+  $(".rules").hide();
+  $(".nav-3").addClass('nav--active')
+  $(".nav-1").removeClass('nav--active')
+  $(".nav-2").removeClass('nav--active')
 });
